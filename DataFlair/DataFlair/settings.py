c0879ead754e082c114c277f6a25053d435dbf72
ranjeetraj198 <56380153+ranjeetraj198@users.noreply.github.com>
2020-05-23 08:03:51 +0530

    Update settings.py

--- DataFlair/DataFlair/settings.py
+++ DataFlair/DataFlair/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'DataFlair.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'student',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
